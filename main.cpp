ea2bb1ee7a5a39c82ae6e729957457f54cce75af
jstanthrctgrl <sotov2070@gmail.com>
2023-05-14 11:25:26 +0300

    IS THIS FINALLY GOING TO WORK?????

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
 	using namespace Trees;
-	std::vector<int> s = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	std::vector<int> s = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 118 };
 	Tree<int> t = construct<int>(s);
 	t.print_recursively();
 
