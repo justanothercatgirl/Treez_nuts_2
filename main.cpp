7e623e85d9ae5f6f3d6d406a93e96dcf74405f92
jstanthrctgrl <sotov2070@gmail.com>
2023-05-15 00:05:51 +0300

    somebody help me please

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-	using namespace Trees;
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie();
 	std::vector<int> s = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 118 };
-	Tree<int> t = construct<int>(s);
+	Trees::Tree<int> t = Trees::construct<int>(s);
 	t.print_recursively();
-
+	std::cin.get();
 }
 
 // cd / d D : / Programming / Git, cmake and linking / project0
